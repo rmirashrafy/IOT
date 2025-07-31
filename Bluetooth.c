c6ac670f972e4c5e122ee63d61300e8370787a8f
F.Mirashrafi <fatemehmira394@gmail.com>
2025-07-31 18:15:55 +0330

    Update Bluetooth.c
    the app that works whit android phone

--- Bluetooth.c
+++ Bluetooth.c
@@ -1,37 +1,44 @@
 #include <BluetoothSerial.h>
-#include "SSD1306.h" // OLED library (same as SSD1306Wire.h)
+#include "SSD1306.h" // OLED library
 
 BluetoothSerial SerialBT;
-
-// OLED display on I2C using pins SDA = GPIO5, SCL = GPIO4
 SSD1306 display(0x3c, 5, 4);
 
-int sensorPin = 36;   // LDR input pin
-int sensorValue = 0;  // Variable to store analog value
-String receivedText = ""; // Text received via Bluetooth
+int sensorPin = 36;
+int sensorValue = 0;
+String receivedText = "";
+
+bool wasConnected = false;  // Track previous connection state
 
 void setup() {
-  // Serial Monitor
   Serial.begin(115200);
-  
-  // Start Bluetooth with name "ESP32_BT"
   SerialBT.begin("ESP32_BT");
   Serial.println("Bluetooth started. Pair with 'ESP32_BT'");
 
-  // OLED setup
   display.init();
   display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_16); // Smaller font to fit both lines
+  display.setFont(ArialMT_Plain_16);
 }
 
 void loop() {
-  // Read LDR sensor
   sensorValue = analogRead(sensorPin);
 
-  // Send sensor value to Bluetooth
-  SerialBT.println("Sensor: " + String(sensorValue));
+  // Check current connection state
+  bool isConnected = SerialBT.hasClient();
 
-  // Check if data is received via Bluetooth
+  // If just connected (was disconnected before)
+  if (isConnected && !wasConnected) {
+    Serial.println("Client connected!");
+    SerialBT.println("You are connected to the container");
+  }
+  wasConnected = isConnected; // Update state
+
+  // Send sensor value only if connected
+  if (isConnected) {
+    SerialBT.println("Sensor: " + String(sensorValue));
+  }
+
+  // Read incoming Bluetooth data
   if (SerialBT.available()) {
     receivedText = SerialBT.readStringUntil('\n');
     Serial.println("Received: " + receivedText);
@@ -42,7 +49,7 @@ void loop() {
   display.setTextAlignment(TEXT_ALIGN_CENTER);
   display.setColor(WHITE);
   display.drawString(64, 0, "LDR: " + String(sensorValue));
-  display.drawString(64, 25, receivedText); // Show received text
+  display.drawString(64, 25, receivedText);
   display.display();
 
   delay(1000);
